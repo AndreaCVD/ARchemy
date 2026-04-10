c6ffcf1e158bf70b01c0fb1813ff18300495292f
AinaMC <aina.mestres@estudiantat.upc.edu>
2026-04-10 14:20:52 +0200

    no hace el void ns pq

--- Assets/raa/Scripts/DetectPlane.cs
+++ Assets/raa/Scripts/DetectPlane.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
@@ -6,10 +7,21 @@ using UnityEngine.XR.ARSubsystems;
 public class DetectPlane : MonoBehaviour
 {
     [SerializeField] private ARPlaneManager planeManager;
+    //[SerializeField]  VRInstancer intanciar;
+    [SerializeField] GameObject prefabObj;
     //[SerializeField] private ARRaycastManager raycastManager;
     //[SerializeField] private GameObject prefabObj;
     private List<ARPlane> detectedPlanes = new List<ARPlane>();
+    public bool planeDetected;
 
+    void Update()
+    {
+        if(planeDetected)
+        {
+            InvokeRepeating("Instance()", 3.0f, 3.0f);
+        }
+
+    }
     //private List<XRRaycastHit> hits = new List<XRRaycastHit>();
     private void OnEnable()
     {
@@ -54,19 +66,27 @@ public class DetectPlane : MonoBehaviour
         foreach (var plane in args.added)
         {
             detectedPlanes.Add(plane);
+            planeDetected = true;
             // Debug.Log("Plano detectado:" + plane.planeId+" en posición"+ plane.center);
             Debug.Log("Plano detectado:" + plane);
-        }
-
-        foreach (var plane in args.updated)
-        {
-            // Actualizar datos del plano si es necesario
         }
 
         foreach (var plane in args.removed)
         {
             detectedPlanes.Remove(plane);
+            planeDetected = false;
+            //intanciar.StopInstance();
             Debug.Log("Plano eliminado:" + plane);
         }
+    }
+
+    void Instance() //Instanciar
+    {
+        Debug.Log("Instancias seta");
+
+        // Instanciar el objeto en la posición del plano
+        //GameObject nuevoObjeto = Instantiate(prefabObj, planePosition, Quaternion.identity);
+        // Opcional: Hacer que el objeto sea hijo de un objeto vacío para organizar la jerarquía
+        //nuevoObjeto.transform.SetParent(transform);
     }
 }
--- Assets/raa/Scripts/VRInstancer.cs
+++ Assets/raa/Scripts/VRInstancer.cs
@@ -6,76 +6,79 @@ public class VRInstancer : MonoBehaviour
 {
     [SerializeField] GameObject prefabObj;
     [SerializeField] float delay = 2.0f; // Segundos entre instancias
-    private bool planeDetected = false; //Si detecta plano o no
+    //[SerializeField] DetectPlane isPlane; //Si detecta plano o no
     private Vector3 planePosition; //Como esta el plano
-
-    [Tooltip("The list of prefabs available to spawn.")]
-    List<GameObject> m_ObjectPrefabs = new List<GameObject>();
-    public List<GameObject> objectPrefabs
-    {
-        get => m_ObjectPrefabs;
-        set => m_ObjectPrefabs = value;
-    }
+    //bool planeDetected;
+    //[Tooltip("The list of prefabs available to spawn.")]
+    //List<GameObject> m_ObjectPrefabs = new List<GameObject>();
+    //public List<GameObject> objectPrefabs
+    //{
+    //    get => m_ObjectPrefabs;
+    //    set => m_ObjectPrefabs = value;
+    //}
 
     void Start()
     {
 
     }
 
-    private IEnumerator Instance(float time) //Instanciar
+    public void Instance() //Instanciar
     {
-        while (planeDetected) //mientras se detecte el plano sea true
-        {
+        //while (planeDetected) //mientras se detecte el plano sea true
+        //{
                 // Instanciar el objeto en la posición del plano
                 GameObject nuevoObjeto = Instantiate(prefabObj, planePosition, Quaternion.identity);
                 // Opcional: Hacer que el objeto sea hijo de un objeto vacío para organizar la jerarquía
                 nuevoObjeto.transform.SetParent(transform);
 
-                yield return new WaitForSeconds(time); // Esperar el tiempo especificado
-        }
+                //yield return new WaitForSeconds(delay); // Esperar el tiempo especificado
+        //}
     }
+    public void StopInstance()
+    {
+        //StopCoroutine(Instance());
+        Debug.Log("Se ha parado las intancias");
 
-
-        
-    void Update() 
-    {
-        PlaneDetection();
-        // Iniciar cuando se detecte plano
-        if (planeDetected)
-        {
-            planeDetected = true;
-            StartCoroutine(Instance(delay));
-        }
-        else
-        {
-            //No detecta ningun plano
-            Debug.Log("No se detecta ningun plano");
-        }
     }
-    void PlaneDetection()//Detectar Plano
-    {
-        Ray ray = new Ray(transform.position, Vector3.down);
-        if (Physics.Raycast(ray, out RaycastHit hit))
-        {
-            if (hit.collider.CompareTag("XR Simulation"))
-            {
-                planeDetected = true;
-                //if (corutinaInstanciado == null)
-                //{
-                //    corutinaInstanciado = InstanciarPeriodicamente(intervaloSegundos);
-                //    StartCoroutine(corutinaInstanciado);
-                //}
-                UpdatePlane(hit.point);
-            }
-            else {
-                planeDetected = false;
-            }
-        }
-    }    
-    // Actualizar posición del plano cuando se detecta con el Raycast
-    public void UpdatePlane(Vector3 newPos)
-    {
-        planePosition = newPos;
-        planeDetected = true;
-    }
+    //void Update() 
+    //{
+    //    // Iniciar cuando se detecte plano
+    //    if (isPlane.planeDetected == true)
+    //    {
+    //        planeDetected = true;
+    //        StartCoroutine(Instance(delay));
+    //    }
+    //    else
+    //    {
+    //        StopCoroutine(Instance(0f));
+    //        //No detecta ningun plano
+    //        Debug.Log("No se detecta ningun plano");
+    //    }
+    //}
+    //void PlaneDetection()//Detectar Plano
+    //{
+    //    Ray ray = new Ray(transform.position, Vector3.down);
+    //    if (Physics.Raycast(ray, out RaycastHit hit))
+    //    {
+    //        if (hit.collider.CompareTag("XR Simulation"))
+    //        {
+    //            planeDetected = true;
+    //            //if (corutinaInstanciado == null)
+    //            //{
+    //            //    corutinaInstanciado = InstanciarPeriodicamente(intervaloSegundos);
+    //            //    StartCoroutine(corutinaInstanciado);
+    //            //}
+    //            UpdatePlane(hit.point);
+    //        }
+    //        else {
+    //            planeDetected = false;
+    //        }
+    //    }
+    //}    
+    //// Actualizar posición del plano cuando se detecta con el Raycast
+    //public void UpdatePlane(Vector3 newPos)
+    //{
+    //    planePosition = newPos;
+    //    planeDetected = true;
+    //}
 }
