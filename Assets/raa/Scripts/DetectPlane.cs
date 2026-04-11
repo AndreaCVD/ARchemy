b7061310d9c79857ec6107dfdc7aa7506110a213
AinaMC <aina.mestres@estudiantat.upc.edu>
2026-04-11 23:29:59 +0200

    el biporus sigue sin funcionar
    creo mejor cambiar el modelo
    Ya que tiene un pivote raro que  no se puede modificar, aunque se haga un Empty este no le hace caso

--- Assets/raa/Scripts/DetectPlane.cs
+++ Assets/raa/Scripts/DetectPlane.cs
@@ -12,7 +12,7 @@ public class DetectPlane : MonoBehaviour
     [SerializeField] private ARPlaneManager planeManager;
     [Header("Lista de Setas Disponibles")]
     public List<GameObject> TiposSetas = new List<GameObject>();
-    [SerializeField] GameObject padre;
+    [SerializeField] GameObject father;
 
     [Header("Planos que se detectan")]
     public List<ARPlane> detectedPlanes = new List<ARPlane>();
@@ -36,16 +36,15 @@ public class DetectPlane : MonoBehaviour
         coroutineStart = false;
         planeDetect = false;
         ray = new Ray(cameraView.transform.position, cameraView.transform.forward);
-
     }
     void Update()
     {
+        //Hacer que el ray siempre salga del punto de vista de la camara
         ray = new Ray(cameraView.transform.position, cameraView.transform.forward);
         if (planeDetect == true && coroutineStart == false) //si la lista no esta vacia es que detecta planos
         {            
             coroutineStart = true;
             Invoke(nameof(Instance), 2.0f);
-            //StartCoroutine(Instance(delay));
             Debug.Log("Corotina Empezada: " + detectedPlanes[0]);
              
         }
@@ -74,30 +73,7 @@ public class DetectPlane : MonoBehaviour
             planeManager.planesChanged -= OnPlanesChanged;
         }
     }
-    //void Update()
-    //{
-    //    //// Verificar si se detectó un plano nuevo o si hay input de usuario
-    //    //if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
-    //    //{
-    //        Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
 
-    //        // Realizar raycast para encontrar planos
-    //        if (raycastManager.Raycast(Input.GetTouch(0).position, hits, TrackableType.PlaneWithinPolygon))
-    //        {
-    //            XRPlaneHit hit = hits[0];
-
-    //            // Colocar objeto en la posición del plano detectado
-    //            if (hit.trackable is XRPlane plane)
-    //            {
-    //                Vector3 position = hit.pose.position;
-    //                Quaternion rotation = hit.pose.rotation;
-
-    //                // Instanciar objeto automáticamente
-    //                Instantiate(prefabObj, position, rotation, transform);
-    //            }
-    //        }
-    //    //}
-    //}
     private void OnPlanesChanged(ARPlanesChangedEventArgs args)
     {
         foreach (var plane in args.added)
@@ -114,31 +90,31 @@ public class DetectPlane : MonoBehaviour
         }
     } 
 
-    void Instance() //Instanciar
+    void Instance() //Instanciar setas
     {
         //Ejecutar Raycast
-        //Distancia máxima (ej. 100 unidades), sino com Mathf.Infinity no tiene limite
+        //Distancia máxima del ray, sino con Mathf.Infinity no tiene limite
         Physics.Raycast(ray, out hitInfo, Mathf.Infinity);
-        //Debug.DrawLine(ray.origin, hitInfo.point, Color.red);
         Debug.Log("ROTATION" + hitInfo.normal); //la normal de la superficie que se encuentra raycast        
         
         if (hitInfo.normal == new Vector3(1, 0, 0)) //Pared
         {
             //TiposSetas seta = TiposSetas[Random.Range(0, TiposSetas.Count)];
-            GameObject newseta = Instantiate(TiposSetas[Random.Range(0, TiposSetas.Count)], hitInfo.point, Quaternion.Euler(90f, 0f, -90f), padre.transform);
-            ListaMushrooms.Add(newseta);
+            //TipoSetas.Count --> directamente coje elementos maximos de la lista
+            GameObject newmush = Instantiate(TiposSetas[Random.Range(0, TiposSetas.Count)], hitInfo.point, Quaternion.Euler(90f, 0f, -90f), father.transform);
+            ListaMushrooms.Add(newmush);
         }
         else if (hitInfo.normal == new Vector3(0, 1, 0)) //Suelo
         {
-            GameObject newseta = Instantiate(TiposSetas[Random.Range(0, TiposSetas.Count)], hitInfo.point, Quaternion.Euler(0f, 90f, 0f), padre.transform);
-            ListaMushrooms.Add(newseta);
+            GameObject newmush = Instantiate(TiposSetas[Random.Range(0, TiposSetas.Count)], hitInfo.point, Quaternion.Euler(0f, 90f, 0f), father.transform);
+            ListaMushrooms.Add(newmush);
         }
 
-
-        if (hitInfo.transform.gameObject.layer == 30) //30 es XR Simulation
-        {
-            Debug.Log("Instancias seta");
-            //ListaMushrooms.Add(prefabObj);
-        }
+        //No funciona ns pq
+        //if (hitInfo.transform.gameObject.layer == 30) //30 es XR Simulation
+        //{
+        //    Debug.Log("Instancias seta");
+        //    //ListaMushrooms.Add(prefabObj);
+        //}
     }
 }
