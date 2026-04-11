40ee17fd09aaf820b8d58a3596ea3eb0a6c3e6c3
AinaMC <aina.mestres@estudiantat.upc.edu>
2026-04-11 20:41:38 +0200

    setas se instancia bien de tiempo, no lugar

--- Assets/raa/Scripts/DetectPlane.cs
+++ Assets/raa/Scripts/DetectPlane.cs
@@ -1,5 +1,5 @@
 using System.Collections.Generic;
-
+using System.Reflection;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
@@ -9,17 +9,49 @@ public class DetectPlane : MonoBehaviour
     [SerializeField] private ARPlaneManager planeManager;
     //[SerializeField]  VRInstancer intanciar;
     [SerializeField] GameObject prefabObj;
+    [SerializeField] GameObject padre;
     //[SerializeField] private ARRaycastManager raycastManager;
     //[SerializeField] private GameObject prefabObj;
     public List<ARPlane> detectedPlanes = new List<ARPlane>();
-    public bool planeDetected;
+    public List<GameObject> ListaMushrooms = new List<GameObject>();
+    //public bool planeDetected;
+    public float delay = 10f;
+    
+    public bool coroutineStart;
+    public bool planeDetect;
+    public float time = 0f;
+    public float timeMax = 10f;
 
+    RaycastHit hitInfo; //Informacion de cuando el raycast de la camara se encuentre con pared layer XRSimulation
+    Ray ray;
+    [SerializeField] Camera cameraView;
+
+    void Start()
+    {
+        coroutineStart = false;
+        planeDetect = false;
+        ray = new Ray(cameraView.transform.position, cameraView.transform.forward);
+
+    }
     void Update()
     {
-        if(detectedPlanes != null) //si la lista no esta vacia es que detecta planos
-        {
-            //InvokeRepeating("Instance()", 3.0f, 3.0f);
-            Debug.Log("--PLANO--: " + detectedPlanes[0].planeId );
+        ray = new Ray(cameraView.transform.position, cameraView.transform.forward);
+        if (planeDetect == true && coroutineStart == false) //si la lista no esta vacia es que detecta planos
+        {            
+            coroutineStart = true;
+            InvokeRepeating(nameof(Instance), 2.0f, delay);
+            //StartCoroutine(Instance(delay));
+            Debug.Log("Corotina Empezada: " + detectedPlanes[0]);
+
+        }
+        else {
+            //coroutineStart= false; --> NO HACER ESTO, PETA
+            time += Time.deltaTime;
+            if ( time >= timeMax)
+            {
+                time = 0f;
+                coroutineStart = false;
+            }
         }
 
     }
@@ -67,6 +99,7 @@ public class DetectPlane : MonoBehaviour
         foreach (var plane in args.added)
         {
             detectedPlanes.Add(plane);
+            planeDetect = true;
             // Debug.Log("Plano detectado:" + plane.planeId+" en posición"+ plane.center);
             Debug.Log("Plano detectado: " + plane);
         }
@@ -77,17 +110,20 @@ public class DetectPlane : MonoBehaviour
             //intanciar.StopInstance();
             Debug.Log("Plano eliminado:" + plane);
         }
-    }
+    } 
 
     void Instance() //Instanciar
     {
         Debug.Log("Instancias seta");
+        ListaMushrooms.Add(prefabObj);
+        Instantiate(prefabObj, cameraView.transform.position, Quaternion.identity, padre.transform);
 
         // Instanciar el objeto en la posición del plano
         //GameObject nuevoObjeto = Instantiate(prefabObj, planePosition, Quaternion.identity);
         // Opcional: Hacer que el objeto sea hijo de un objeto vacío para organizar la jerarquía
         //nuevoObjeto.transform.SetParent(transform);
     }
+
 
 //    // Ejemplo correcto para obtener un TrackableId válido:
 //    List<ARRaycastHit> _hits = new List<ARRaycastHit>();
